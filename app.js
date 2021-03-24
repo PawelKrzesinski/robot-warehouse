66e7a6da6b548c4d30486460a34bc94473a4d43e
Pawel <pawel.krzesinski@oneserve.co.uk>
2021-03-24 17:03:50 +0000

    added pick the crate function, solved issues with vanishing crates, tidied up code in few places

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ const vacant = "white";
 const crateCol = "brown"
 const robotColour = "black"
 
-function drawSquare(x, y, color){
+function drawSquare(x, y, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x*sq, y*sq, sq, sq);
+    ctx.fillRect(x * sq, y * sq, sq, sq);
     ctx.strokeStyle = "black";
-    ctx.strokeRect(x*sq, y*sq, sq, sq)
+    ctx.strokeRect(x * sq, y * sq, sq, sq)
 }
 
 //create a board
-let board = [ ];
-for(let r = 0; r < row; r++){
-    board[r] = [ ];
-    for(let c = 0; c < col; c++){
+let board = [];
+for (let r = 0; r < row; r++) {
+    board[r] = [];
+    for (let c = 0; c < col; c++) {
         board[r][c] = vacant;
     }
 }
 
 //draw the board
-function drawBoard(){
-    for(r = 0; r < row; r++){
-        for(c = 0; c < col; c++){
+function drawBoard() {
+    for (r = 0; r < row; r++) {
+        for (c = 0; c < col; c++) {
             drawSquare(c, r, board[r][c])
         }
     }
@@ -47,105 +47,142 @@ let robot = {
 drawSquare(robot.x, robot.y, robot.color)
 
 //Function for robot's movement
-function moveRobot(){
+function moveRobot() {
     input.addEventListener("keydown", () => {
-        if(event.keyCode == 13){
-            const commands = input.value.split("");
-            getDirection(commands)
+        if (event.keyCode == 13) {
+            const receivedCommands = input.value.split("");
+            getDirection(receivedCommands)
             input.value = "";
         }
     });
-    
 }
 
 moveRobot()
 
-function getDirection(commands){
-    const directions = ["N", "S", "W", "E"];
-	// Tidies up any whitespace
-    commands.forEach((command, index) => {
-        if(directions.includes(command)){
-            console.log("I got the direction", command);
-            if(command === "N"){
-                if (robot.y > 0){
-                    drawSquare(robot.x, robot.y, vacant);
-                    robot.y -= 1;
-                    checkIfNotVacant()
-                    drawSquare(robot.x, robot.y, robot.color);
-                } else {
-                    return false;
-                }
-            } else if(command === 'S'){
-                if (robot.y < 9){
-                    drawSquare(robot.x, robot.y, vacant);
-                    robot.y += 1;
-                    checkIfNotVacant()
-                    drawSquare(robot.x, robot.y, robot.color);
-                } else {
-                    return false;        
-                }
-            } else if(command === 'W'){
-                if (robot.x > 0){
-                    drawSquare(robot.x, robot.y, vacant);
-                    robot.x -= 1;
-                    checkIfNotVacant()
-                    drawSquare(robot.x, robot.y, robot.color);
-                } else {
-                    return false;
-                }
-            } else if(command === 'E'){
-                if (robot.x < 9){
-                    drawSquare(robot.x, robot.y, vacant);
-                    robot.x += 1;
-                    
-                    checkIfNotVacant()
-                    drawSquare(robot.x, robot.y, robot.color);
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            console.log(`Error! Got a wrong command: "${command}", needs tidying`);
-            commands.splice(index, 1);
-            console.log("Tidying up unwanted commands");
-        }
-        //something here
-    })
-
-}
-
-function Crate(x, y){
+function Crate(x, y) {
     this.x = x;
     this.y = y;
     drawSquare(x, y, crateCol)
 }
 let crateArr = [];
-function createCrates(coordinates){
+function createCrates(coordinates) {
     crateArr = [...coordinates];
-    
-    crateArr.forEach( crate => {
-        crate = new Crate(crate[0], crate[1])
+    crateArr.forEach(crate => {
+        crate = new Crate(crate.x, crate.y)
     })
     return crateArr;
 }
-createCrates([[5, 5], [9, 0]]);
+createCrates([{ x: 5, y: 5 }, { x: 9, y: 0 }]);
 
-function checkIfNotVacant(){
-    crateArr.forEach( crate => {
-        if(robot.x === crate[0] && robot.y === crate[1]){
-            alert("there is a crate here!")
+
+
+
+function getDirection(receivedCommands) {
+    const commands = ["N", "S", "W", "E", "G", "D"];
+    // Tidies up any whitespace
+    receivedCommands.forEach((command, index) => {
+        if (commands.includes(command)) {
+            console.log("I got the direction", command);
+            if (command === "N") {
+                if (robot.y > 0) {
+                    drawSquare(robot.x, robot.y, vacant);
+                    robot.y -= 1;
+                    drawSquare(robot.x, robot.y, robot.color);
+                } else {
+                    return false;
+                }
+            } else if (command === 'S') {
+                if (robot.y < 9) {
+                    drawSquare(robot.x, robot.y, vacant);
+                    robot.y += 1;
+                    drawSquare(robot.x, robot.y, robot.color);
+                } else {
+                    return false;
+                }
+            } else if (command === 'W') {
+                if (robot.x > 0) {
+                    drawSquare(robot.x, robot.y, vacant);
+                    robot.x -= 1;
+                    drawSquare(robot.x, robot.y, robot.color);
+                } else {
+                    return false;
+                }
+            } else if (command === 'E') {
+                if (robot.x < 9) {
+                    robot.x += 1;
+                    let wasOnCrate = crateArr.some(crate => crate.x == robot.x-1 && crate.y == robot.y)
+                    let isOnCrate = crateArr.some(crate => crate.x == robot.x && crate.y == robot.y)
+                    //if crate is picked undraw it previous position
+                    if(isOnCrate){
+                        drawSquare(robot.x, robot.y, "chocolate");
+                        drawSquare(robot.x - 1, robot.y, vacant);
+                    } else if(wasOnCrate){
+                        drawSquare(robot.x, robot.y, robot.color);
+                        drawSquare(robot.x - 1, robot.y, crateCol);
+                    } else {
+                        drawSquare(robot.x, robot.y, robot.color);
+                        drawSquare(robot.x - 1, robot.y, vacant);
+                    }
+                    crateArr.forEach(crate => {
+                        if(robot.lifting){
+                            crate.x += 1;
+                            drawSquare(robot.x - 1, robot.y, vacant);
+                        }
+                    })
+                    console.log(crateArr);
+                } else {
+                    console.log("Path blocked, you hit the wall");
+                    return false;
+                }
+            } else if (command === 'G') {
+                if(!robot.lifting){
+                    pickTheCrate()
+                } else {
+                    console.log("I am already lifting something!")
+                }
+                
+            } else if (command === 'D') {
+                dropTheCrate()
+            }
+        } else {
+            console.log(`Error! Got a wrong command: "${command}", needs tidying`);
+            receivedCommands.splice(index, 1);
+            console.log("Tidying up unwanted receivedCommands");
         }
     })
 }
 
-function pickTheCrate(){
-    robot.color = "darkgray";
-    robot.lifting = true;
+
+
+
+function pickTheCrate() {
+    let isCratePresent = crateArr.some(crate => crate.x === robot.x && crate.y === robot.y)
+    if(isCratePresent){
+        console.log("Picking up a crate!");
+        robot.lifting = true;
+        robot.color = "darkgray";
+        drawSquare(robot.x, robot.y, robot.color);
+    } else {
+        console.log("No crates to pick up!");
+    }
+}
+
+function dropTheCrate() {
+    crateArr.forEach(crate => {
+        if (robot.lifting === true && crate.x === robot.x && crate.y === robot.y) {
+            robot.color = robotColour;
+            robot.lifting = false;
+            drawSquare(robot.x, robot.y, crateCol)
+        } else {
+            console.log("You are not lifting anything!");
+        }
+    })
 }
 
 
+
 // The robot is equipped with a lifting claw which can be used to move crates around the warehouse. We track the locations of all the crates in the warehouse.
-// Extend the robot's commands to include the following:
+// Extend the robot's receivedCommands to include the following:
 // G grab a crate and lift it
 // D drop a crate gently to the ground
 // There are some rules about moving crates:
